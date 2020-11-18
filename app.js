6896fee552f2c3829caf8e9637e255ff9946bba5
itsamananand29 <itsamananand.29@gmail.com>
2020-11-18 17:57:41 +0530

    firebase implemented

--- app.js
+++ app.js
@@ -49,7 +49,6 @@ const compress= ()=>{
         reader.onload= (e)=>{
         const inputImage = document.createElement('img'); // creating an img element using the document object model function 
         inputImage.src = e.target.result;   // set the image to be displayed uploaded by the user
-        document.querySelector('#input').src = e.target.result;
         // console.log(e.target.result.split(','));
         
         // get the base64 name of the file 
@@ -57,8 +56,20 @@ const compress= ()=>{
         
         // get the reference of the firebase storage bucket and store your file 
         let fileName = new Date()+'-image';
-        firebase.storage().ref().child(fileName).putString(base64Name, 'base64', {contentType:'image/jpg'}); 
-        
+        firebase.storage().ref().child(fileName).putString(base64Name, 'base64', {contentType:'image/jpg'}) 
+        .then(snapshot =>{  
+            //console.log(snapshot.ref._delegate._location.path_);
+            
+            //retrive the data from firebase 
+            var ref0 = firebase.storage().ref().child(snapshot.ref._delegate._location.path_);
+            // Get the download URL
+            ref0.getDownloadURL().then(function(url) {
+            // Insert url into an <img> tag to "download"
+            document.querySelector('#input').src=url;// set the image as the source of the output image
+            }).catch(function(error) {
+                console.log(error.code)
+            });
+        });
         // start the resizing 
         inputImage.onload = (e1)=>{   
             const outputImage1 = document.createElement('canvas'); // creating a canvas element using createElement method 
@@ -68,15 +79,26 @@ const compress= ()=>{
             const contxt = outputImage1.getContext('2d');  // getContext is a dom function to get the rendering canvas element and its functions 
             contxt.drawImage(e1.target, 0, 0, outputImage1.width, outputImage1.height); // to draw the image on the context element , drawImage(image,x-axis,y-axis,width,height)
             const srcEncoded = contxt.canvas.toDataURL(e1.target,'image10/jpeg') // this will the image in the specified format 
-            document.querySelector('#output').src=srcEncoded;  // set the image as the source of the output image
-           
+            
             // get the base64 name of the file 
             const base64Name1 = srcEncoded.toString().split(',')[1];
          
             // get the reference of the firebase storage bucket and store your file 
             let fileName1 = new Date()+'-image_10';
-            firebase.storage().ref().child(fileName1).putString(base64Name1, 'base64', {contentType:'image/jpg'});
-
+            firebase.storage().ref().child(fileName1).putString(base64Name1, 'base64', {contentType:'image/jpg'})
+            .then(snapshot =>{  
+                //console.log(snapshot.ref._delegate._location.path_);
+                
+                //retrive the data from firebase 
+                var ref1 = firebase.storage().ref().child(snapshot.ref._delegate._location.path_);
+                // Get the download URL
+                ref1.getDownloadURL().then(function(url) {
+                // Insert url into an <img> tag to "download"
+                document.querySelector('#output').src=url;// set the image as the source of the output image
+                }).catch(function(error) {
+                    console.log(error.code)
+                });
+            });
             const outputImage2 = document.createElement('canvas');// creating a canvas element using createElement method
             // reduce the image size by 20
             outputImage2.width = e1.target.width/20; 
@@ -84,17 +106,32 @@ const compress= ()=>{
             const contxt2 = outputImage2.getContext('2d');// getContext is a dom function to get the rendering canvas element and its functions
             contxt2.drawImage(e1.target, 0, 0, outputImage2.width, outputImage2.height);// to draw the image on the context element , drawImage(image,x-axis,y-axis,width,height)
             const srcEncoded2 = contxt2.canvas.toDataURL(e1.target,'image20/jpeg')// this will the image in the specified format 
-            document.querySelector('#output2').src=srcEncoded2;// set the image as the source of the output image
+            
             // console.log(srcEncoded2.split(',')[1]);
             
             // get the base64 name of the file
             const base64Name2 = srcEncoded2.toString().split(',')[1];
             let fileName2 = new Date()+'-image_20';
             // get the reference of the firebase storage bucket and store your file 
-            firebase.storage().ref().child(fileName2).putString(base64Name2, 'base64', {contentType:'image/jpg'});
+            firebase.storage().ref().child(fileName2).putString(base64Name2, 'base64', {contentType:'image/jpg'})
+            .then(snapshot =>{  
+                //console.log(snapshot.ref._delegate._location.path_);
+                
+                //retrive the data from firebase 
+                var ref2 = firebase.storage().ref().child(snapshot.ref._delegate._location.path_);
+                // Get the download URL
+                ref2.getDownloadURL().then(function(url) {
+                // Insert url into an <img> tag to "download"
+                document.querySelector('#output2').src=url;// set the image as the source of the output image
+                }).catch(function(error) {
+                    console.log(error.code)
+                });
+            });
+       
         }    
         
     }
     }
     
 }
+
